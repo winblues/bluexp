1d2474f42473434c73ddfdfd205b279619e9e120
Adam Fidell <adam@fidel.id>
2025-02-22 10:05:56 -0600

    fix: remove old vauxite container registry conf

--- files/scripts/20-xfce-winxp-tc.sh
+++ files/scripts/20-xfce-winxp-tc.sh
@@ -2,17 +2,25 @@
 
 set -xueo pipefail
 
+# Ideally, we would build and package this outside of the image build process.
+# However, it's probably not possible to package this in on Fedora's COPR
+# because of licensing concerns with some of the assets
+#
+# TODO: package this using a separate GitHub repo maybe?
+
 dnf5 install -y $(cat packages/xfce-winxp-tc/build-deps.txt)
 
 XFCE_WINXP_TC_VERSION="4f73e4a740041635eecd30abccc2fdafabb21582"
 
 mkdir -p /tmp/xfce-winxp-tc
 cd /tmp/xfce-winxp-tc
-git clone --depth 1 https://github.com/rozniak/xfce-winxp-tc.git
+git clone https://github.com/rozniak/xfce-winxp-tc.git
 cd xfce-winxp-tc
 git checkout $XFCE_WINXP_TC_VERSION
 bash packaging/buildall.sh
 
 rpm-ostree install xptc/*/rpm/std/x86_64/fre/wintc-*.rpm
+
+# TODO: remove build packages
 
 plymouth-set-default-theme bootvid
