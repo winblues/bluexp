152dc215b4e7733278b16facee3dc611de522f39
Adam Fidel <adam@fidel.id>
2025-03-14 00:28:51 -0500

    feat: add JetBrains Mono nerd font

--- files/scripts/01-main.sh
+++ files/scripts/01-main.sh
@@ -4,3 +4,11 @@ set -ouex pipefail
 
 systemctl enable libvirtd.service
 systemctl enable winblues-user-init.service
+
+# Nerd fonts
+curl -Lo /tmp/jetbrains.tar.xz https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.tar.xz
+cd /usr/share/fonts
+mkdir jetbrains-mono-nerdfonts
+cd jetbrains-mono-nerdfonts
+tar xf /tmp/jetbrains.tar.xz
+fc-cache -v
