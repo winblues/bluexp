8d86565947e724f97367fa4e5b13ddef88437993
Adam Fidel <adam@fidel.id>
2026-03-18 16:57:21 -0500

    Use fedora version in oci tags

--- files/scripts/20-xfce-winxp-tc.sh
+++ files/scripts/20-xfce-winxp-tc.sh
@@ -10,7 +10,8 @@ dnf5 install -y golang-oras
 RPM_DIR=$(mktemp -d)
 cd "$RPM_DIR"
 
-oras pull "ghcr.io/winblues/xfce-winxp-tc-rpms:${XFCE_WINXP_TC_VERSION}"
+FEDORA_MAJOR_VERSION=$(awk -F= '/^VERSION_ID/ {print $2}' /etc/os-release)
+oras pull "ghcr.io/winblues/xfce-winxp-tc-rpms:${XFCE_WINXP_TC_VERSION}-${FEDORA_MAJOR_VERSION}"
 
 dnf5 install ./*.rpm
 
